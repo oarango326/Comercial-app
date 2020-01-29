a654be74c4dc2b32a1339503102e019dce56fdc7
Oscar Arango <oscar.arango326@gmail.com>
2020-01-29 15:43:46 +0100

    correcion de cobros en produccion 1 por fallo en llamada ajax a facturas pendientes 3

--- resources/views/cobros/index.blade.php
+++ resources/views/cobros/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layout')
 @section('contenido')
 <div class="container">
+    @if(session()->has('info'))
+    <div id="session" class="alert alert-success" role="alert">
+        <span>{{session('info')}}</span>
+    </div>
+    @endif
     <div class="row">
         <div class="col-xs-12 col-md-3 col-sm-3 col-lg-3 d-flex justify-content-start">
             <h3 style="margin:5px">Cobros</h3>
@@ -17,11 +22,7 @@
     <hr>
     <h3>No hay Registros</h3>
     @else
-    @if(session()->has('info'))
-    <div id="session" class="alert alert-success" role="alert">
-        <span>{{session('info')}}</span>
-    </div>
-    @endif
+
     <div class="row justify-content-center ">
         <table class="table table-striped table-responsive-xl table-responsive-sm">
             <thead>
